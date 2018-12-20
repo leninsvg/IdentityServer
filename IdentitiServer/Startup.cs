ee5723aff769025b7fa03c0d41e250d3cb693a11
Lenin <leninsvg@gmail.com>
2018-12-20 18:10:22 -0500

    Add project files.

---
+++ IdentitiServer/Startup.cs
@@ -0,0 +1,46 @@
+﻿
+using ApiClient.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ApiClient
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<BankContext>(options => 
+				options.UseSqlServer(Configuration.GetConnectionString("BankingDbContection")));
+			//services.AddDbContext<BankContext>(options =>
+			//	options.UseInMemoryDatabase("BankingDbContection"));
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
+
+
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+	}
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+	public class Config
+	{
+		public static IEnumerable<ApiResource> GetAllApiResources()
+		{
+			return new List<ApiResource>()
+			{
+				new ApiResource("ApiResource", "Customer of api for bankOfDotNetApi")
+			};
+		}
+
+		public static IEnumerable<Client> GetClients()
+		{
+			return new List<Client>()
+			{
+				new Client()
+				{
+					ClientId = "client1",
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
+					ClientSecrets =
+					{
+						new Secret("secret".Sha256())
+					},
+					AllowedScopes = { "ApiResource" }
+				}
+			};
+		}
+	}
+}
